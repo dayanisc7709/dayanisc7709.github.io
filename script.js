bfdaa04b491d1a9d80017717d39abd540943c018
dayanisc7709 <146861844+dayanisc7709@users.noreply.github.com>
2023-10-03 11:56:24 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "Dayanis"; // update the part BETWEEN the "quotes"
+let userName = "dayanisc7709"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
